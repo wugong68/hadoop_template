e6e36ca86cddff3cd2e85c810663826d5d6df7dc
admin1 <cc@cc.com>
2021-09-09 22:25:39 +0800

    how to get filename

--- src/main/java/com/hdp/mapper/WordCountMapper.java
+++ src/main/java/com/hdp/mapper/WordCountMapper.java
@@ -3,6 +3,7 @@ package com.hdp.mapper;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
@@ -10,6 +11,18 @@ import java.io.IOException;
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
     final Text text = new Text();
     final IntWritable one = new IntWritable(1);
+
+
+    //how to remeber filename
+    @Override
+    protected void setup(Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+            throws IOException, InterruptedException {
+        // TODO Auto-generated method stub
+        // super.setup(context);
+        FileSplit split = (FileSplit) context.getInputSplit();
+        String filename = split.getPath().getName();
+        System.out.println("current file name :"+filename);
+    }
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
